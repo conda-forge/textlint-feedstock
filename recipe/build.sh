4ce26089c600a60df3991658355110063fa386a5
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-20 12:38:23 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -13,7 +13,14 @@ npm install -ddd \
 pnpm install
 pnpm-licenses generate-disclaimer --prod --output-file=third-party-licenses.txt
 
+mkdir -p ${PREFIX}/bin
+tee ${PREFIX}/bin/textlint << EOF
+#!/bin/sh
+exec \${CONDA_PREFIX}/lib/node_modules/textlint/bin/textlint.js "\$@"
+EOF
+chmod +x ${PREFIX}/bin/textlint
+
 # Create batch wrapper
 tee ${PREFIX}/bin/textlint.cmd << EOF
-call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\bin\textlint %*
+call %CONDA_PREFIX%\bin\node %CONDA_PREFIX%\lib\node_modules\textlint\bin\textlint.js %*
 EOF
