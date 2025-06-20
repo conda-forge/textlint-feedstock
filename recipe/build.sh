e452af5c4670a55c8074dd888ae37871f60e6381
Daniel Nachun <daniel.nachun@gmail.com>
2025-06-20 13:02:31 -0700

    fix build

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,6 +5,7 @@ set -o xtrace -o nounset -o pipefail -o errexit
 # Create package archive and install globally
 npm pack --ignore-scripts
 npm install -ddd \
+    --no-bin-links \
     --global \
     --build-from-source \
     ${PKG_NAME}-${PKG_VERSION}.tgz
